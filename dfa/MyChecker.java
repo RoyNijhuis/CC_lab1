e2f7332db6aeb6ebbe3911d9e922d74d41d92ea3
RoyNijhuis <roynijhuis@hotmail.nl>
2016-04-20 11:38:52 +0200

    Fixed antlr

--- dfa/MyChecker.java
+++ dfa/MyChecker.java
@@ -6,6 +6,15 @@ package dfa;
 public class MyChecker implements Checker {
     @Override
     public boolean accepts(State start, String word) {
+        for (int i=0; i<word.length();i++)
+        {
+            start.getNext(word.charAt(i));
+            if (start.isAccepting())
+            {
+                return true;
+            }
+        }
+
         return false;
     }
 }
